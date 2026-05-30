9d508a0e2b3c221bb58e5688b9446aba93a1e155
Casper da Costa-Luis <casper.dcl@physics.org>
2026-05-30 22:03:21 +0100

    basic framework

---
+++ tests/test_envwrap.py
@@ -0,0 +1,19 @@
+import os
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def set_env():
+    os.environ["FOO_A"] = "42"
+    os.environ["FOO_C"] = "1337"
+
+
+def test_envwrap():
+    from envwrap import envwrap
+
+    @envwrap("FOO_")
+    def test(a=1, b=2, c=3):
+        return {"a": a, "b": b, "c": c}
+
+    assert test(c=99) == {"a": 42, "b": 2, "c": 99}
